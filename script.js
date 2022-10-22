13d868b39249a796d05dc3b0485fcf0dca0a1ff9
blloop <42186044+blloop@users.noreply.github.com>
2022-10-22 15:03:51 -0700

    add background json data

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+// Create structure to store data locally
+var data_salad = {
+
+    // JSON to store values to compute
+    items: {},
+
+    // Save and load data from browser
+    save: () => {
+      localStorage.setItem('data_salad', JSON.stringify(data_salad));
+    }, 
+    load: () => {
+        data_salad = localStorage.getItem('data_salad'); 
+        if (data_salad == null) {
+            data_salad = {};
+        } else {
+            data_salad = JSON.parse(data_salad);
+        }
+    },
+
+    init: () => {
+        info_salad.load();
+        info_salad.list();
+    },
+
+    list: () => {}
+
+}
+
 function displayText() {
     alert("You clicked a button!");
 }
