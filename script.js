1c41f51e733707bd093ef0a1a8057afaed5a5921
blloop <42186044+blloop@users.noreply.github.com>
2022-10-22 23:43:07 -0700

    develop ui, add 2nd calculation

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
-// console.log("Hello");
-
-function testfunc() {
-    document.body.innerHTML = "I";
-}
-
-const getTabName = (tabTitle) => {
-    const retName = `The title of this tab is: ${tabTitle}`
-    return retName;
-}
-
+// Load content upon extension click
 document.addEventListener('DOMContentLoaded', () => {
-  const output = document.getElementById('output');
 
-  // let http_request = '';
+  const output = document.getElementById('output');
+  const output2 = document.getElementById('output');
   chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+
     let http_request = 'http://127.0.0.1:5000/query/?url=' + tabs[0].url
-    output.innerHTML = http_request;
+    // output.innerHTML = http_request;
     
     fetch(http_request)
       .then(response => {
         return response.json();
       })
       .then(function (jsonResponse) {
-        output.innerHTML = jsonResponse.bytes
+        output.innerHTML = jsonResponse.bytes;
+        let calculation = (jsonResponse.bytes * 0.00000004242 * 0.00248138957);
+        output2.innerHTML = calculation;
       })
       .catch(error => {
-        output.innerHTML = error
+        output.innerHTML = "Failed to fetch data!"
       })
+
   });
 
 });
