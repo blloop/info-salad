83e916259b9cea2df6c790934537e02ccadea9b1
blloop <42186044+blloop@users.noreply.github.com>
2022-10-22 23:04:46 -0700

    implement flask server

--- script.js
+++ script.js
@@ -10,74 +10,23 @@ const getTabName = (tabTitle) => {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const output = document.getElementById('output');
-    const query = { 
-        active: true, currentWindow: true 
-    };
+  const output = document.getElementById('output');
 
-    chrome.tabs.query(query, (tabs) => {
-        output.innerHTML = getTabName(tabs[0].title);
-    });
-
-    // --------------------------
-
-    // var protocolVersion = '1.0';
-    // chrome.debugger.attach({
-    //     tabId: tabId
-    // }, protocolVersion, function() {
-    //     if (chrome.runtime.lastError) {
-    //         console.log(chrome.runtime.lastError.message);
-    //         return;
-    //     }
-    //     // Debugger is attached
-    //     chrome.debugger.sendCommand({
-    //         tabId: tabId
-    //     }, "Network.enable", {}, function(response) {
-    //         // Possible response: response.id / response.error
-    //         // 3. Change the User Agent string!
-    //         chrome.debugger.sendCommand({
-    //             tabId: tabId
-    //         }, "Network.responseReceived", {
-    //             userAgent: 'Whatever you want'
-    //         }, function(response) {
-    //             // Possible response: response.id / response.error
-    //             // 4. Now detach the debugger (this restores the UA string).
-    //             chrome.debugger.detach({tabId: tabId});
-    //         });
-    //     });
-    // });
-
-    // --------------------------
-});
-
-// chrome.devtools.network.onRequestFinished.addListener(request => {
-//     document.getElementById("counter").innerHTML += "I";
+  // let http_request = '';
+  chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+    let http_request = 'http://127.0.0.1:5000/query/?url=' + tabs[0].url
+    output.innerHTML = http_request;
     
-//     request.getContent((body) => {
-//         if (request.request && request.request.url) {
-//             var bodyObj = JSON.parse(body);
-//             console.log(bodyObj);
-//             document.getElementById("counter").innerHTML += "I";
-//         }
-//     });
-// });
+    fetch(http_request)
+      .then(response => {
+        return response.json();
+      })
+      .then(function (jsonResponse) {
+        output.innerHTML = jsonResponse.bytes
+      })
+      .catch(error => {
+        output.innerHTML = error
+      })
+  });
 
-const resources = {};
-page._client.on('Network.dataReceived', (event) => {
-  const request = page._networkManager._requestIdToRequest.get(
-    event.requestId
-  );
-  if (request && request.url().startsWith('data:')) {
-    return;
-  }
-  const url = request.url();
-  const length = event.dataLength;
-  if (url in resources) {
-    resources[url] += length;
-  } else {
-    resources[url] = length;
-  }
 });
-
-const totalUncompressedBytes = Object.values(resources).reduce((a, n) => a + n, 0);
-document.getElementById("counter").innerHTML += totalUncompressedBytes;
--- web_scraper.py
+++ web_scraper.py
@@ -1,24 +1,27 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 from bs4 import BeautifulSoup
 import requests
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def home():
     return "Empty URL!"
 
 @app.route('/query/')
-def scrape_url():
+def scraped():
     url = request.args.get('url')
     print(str(url))
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
     text = (soup.get_text().strip())
     bytes = len(str(text)) + 1
+
     # CO_two = (bytes * 0.00000000006 * 707)
-    # print(bytes)
-    return((str(bytes)))
+
+    return(jsonify({'bytes' : bytes}))
     
 if __name__ == "__main__":
     app.run(debug=True)
